ce91d74716b2e84e7e75abd70fe9a84d809e4121
Ronald Mutua <ronaldmutua617@gmail.com>
2023-09-27 10:14:24 -0700

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # using puppet
 
-file { '/etc/ssh/ssh_config':
-	ensure => present,
+file { '/etc/.ssh/ssh_config':
+  ensure => present,
 
 content =>"
-	host*
-	PasswordAuthentication no
-	IdentityFile ~/.ssh/school
-	",
+  host*
+  PasswordAuthentication no
+  IdentityFile ~/.ssh/school
+  ",
 }
