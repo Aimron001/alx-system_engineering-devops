0e0fc7edb2ab7d3a9819464742bc677af2958484
Ronald Mutua <ronaldmutua617@gmail.com>
2023-12-22 02:10:43 -0800

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
 """Function to print top posts on a given Reddit subreddit."""
 import requests
+import sys
+import base64
 
 
 def top_ten(subreddit):
     """Prints the titles of the 10 hottest posts"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    client_id = 'beIUiJlOBKOUF_WAKMba8A'
+    client_secret = '9huuf_Qz3J00w9IiScHyBd2wFY-7fg'
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    auth = 'Basic {}'.format(
+        base64.b64encode('{}:{}'.format(client_id, client_secret).encode()
+                         ).decode()
+    )
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+            'User-Agent': 'An alx api advanced  project/1.0',
+            'Authorization': auth
+        }
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
         print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
